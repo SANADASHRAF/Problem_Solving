66c587998c95359c3e04fb837360271dd6cf8e95
John Doe <john.doe@example.com>
2024-12-11 04:08:43 +0200

    kids_with_the_greatest_number_of_candies

--- Leetcode75/TestProgram.cs
+++ Leetcode75/TestProgram.cs
@@ -6,10 +6,18 @@ namespace Leetcode75
     {
         static void Main(string[] args)
         {
-          
+
             //MergeStringAlternately p1= new MergeStringAlternately();
             //greatest_common_divisor_of_strings x=new greatest_common_divisor_of_strings();
             //Console.WriteLine( x.GcdOfStrings("ABCABC", "ABC") );
+            
+            
+            kids_with_the_greatest_number_of_candies x =new kids_with_the_greatest_number_of_candies();
+            int[] candies = { 2, 3, 5, 1, 3 };
+            int extraCandies = 3;
+            IList<bool> output = x.KidsWithCandies(candies, extraCandies);
+            Console.WriteLine(string.Join(", ", output));
+            
         }
     }
 }
---
+++ Leetcode75/kids-with-the-greatest-number-of-candies.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode75
+{
+    public class kids_with_the_greatest_number_of_candies
+    {
+        //https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/description/?envType=study-plan-v2&envId=leetcode-75
+        public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            int maxinarr = candies.Max();
+            bool[] result = new bool[candies.Length];
+            for (int i = 0; i < candies.Length; i++)
+            {
+                if (candies[i] + extraCandies >= maxinarr)
+                    result[i] = true;
+                else
+                    result[i] = false;
+            }
+            return result; 
+        }
+    }
+}
