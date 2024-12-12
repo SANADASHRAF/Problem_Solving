51cc74d79edcca5bb3b166a01442613d5b1cefda
John Doe <john.doe@example.com>
2024-12-12 15:02:13 +0200

    is-subsequence

---
+++ Leetcode75/is-subsequence.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode75
+{
+    public class is_subsequence
+    {
+        //https://leetcode.com/problems/is-subsequence/description/?envType=study-plan-v2&envId=leetcode-75
+        public bool IsSubsequence(string s, string t)
+        {
+            if(s=="") return true;
+            int sindexincrement = 0;
+            for(int i = 0 ;i < t.Length; i++)
+            {
+                if(sindexincrement < s.Length && s[sindexincrement] == t[i])
+                {
+                    sindexincrement++;
+                    if(sindexincrement == s.Length)
+                        return true;
+                }
+            }
+            return false;
+        }
+    }
+}
--- Leetcode75/obj/Debug/net8.0/Leetcode75.AssemblyInfo.cs
+++ Leetcode75/obj/Debug/net8.0/Leetcode75.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Leetcode75")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19beda8d371c268337d072bea933027aee3c2c3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18c94377aa462db5e541f273ad98236f11586899")]
 [assembly: System.Reflection.AssemblyProductAttribute("Leetcode75")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Leetcode75")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
