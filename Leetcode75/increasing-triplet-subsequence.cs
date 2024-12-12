19beda8d371c268337d072bea933027aee3c2c3e
John Doe <john.doe@example.com>
2024-12-12 14:49:28 +0200

    increasing-triplet-subsequence

---
+++ Leetcode75/increasing-triplet-subsequence.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode75
+{
+    public class increasing_triplet_subsequence
+    {
+        //https://leetcode.com/problems/increasing-triplet-subsequence/description/?envType=study-plan-v2&envId=leetcode-75
+        public bool IncreasingTriplet(int[] nums)
+        {
+            int first = int.MaxValue;
+            int scond = int.MaxValue;
+            foreach (var x in nums)
+            {
+                if(x <= first)
+                   first = x;
+                else if(x<= scond)
+                   scond = x;
+                else 
+                    return true;
+            }
+            return false;
+        }
+    }
+}
