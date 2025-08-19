992ff6f761819b5037cb1c4816b14c2b1d9176fe
SANADASHRAF <sanadbareen@gmai.com>
2025-08-19 15:22:58 +0300

    Determine_if_String_Halves_Are_Alike

--- Leetcode75/obj/Debug/net8.0/Leetcode75.AssemblyInfo.cs
+++ Leetcode75/obj/Debug/net8.0/Leetcode75.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Leetcode75")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18c94377aa462db5e541f273ad98236f11586899")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51cc74d79edcca5bb3b166a01442613d5b1cefda")]
 [assembly: System.Reflection.AssemblyProductAttribute("Leetcode75")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Leetcode75")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Problems/Determine if String Halves Are Alike.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    public class Determine_if_String_Halves_Are_Alike
+    {
+        //https://leetcode.com/problems/determine-if-string-halves-are-alike/description/
+        public bool HalvesAreAlike(string s)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            int RCount = 0, LCount = 0;
+
+            s =s.ToLower();
+
+            int n = s.Length / 2;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (vowels.Contains(s[i]) )
+                {
+                    RCount++;
+                }
+                if (vowels.Contains(s[i + n]))
+                {
+                    LCount++;
+                }
+            }
+            return (RCount == LCount) ? true : false;
+
+            }
+    }
+}
