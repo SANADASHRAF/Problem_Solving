257d40a48a329bad3157213110bd7bb99c721bd4
SANADASHRAF <sanadbareen@gmai.com>
2025-08-20 15:02:02 +0300

    Minimum_Sum_of_Four_Digit_Number_After_Splitting_Digits

--- Leetcode75/obj/Debug/net8.0/Leetcode75.AssemblyInfo.cs
+++ Leetcode75/obj/Debug/net8.0/Leetcode75.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Leetcode75")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51cc74d79edcca5bb3b166a01442613d5b1cefda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+992ff6f761819b5037cb1c4816b14c2b1d9176fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Leetcode75")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Leetcode75")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Problems/Minimum Sum of Four Digit Number After Splitting Digits.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    public class Minimum_Sum_of_Four_Digit_Number_After_Splitting_Digits
+    {
+        //https://leetcode.com/problems/minimum-sum-of-four-digit-number-after-splitting-digits/description/
+        public int MinimumSum(int num)
+        {
+            char[] nums = num.ToString().ToCharArray();
+            Array.Sort(nums);
+            string first = $"{nums[0]}{nums[2]}";
+            string second = $"{nums[1]}{nums[3]}";
+            return int.Parse(first) + int.Parse(second);
+
+        }
+    }
+}
