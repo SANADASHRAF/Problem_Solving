0ff59b999771d5e913ca0bbf7e6548986de66386
SANADASHRAF <sanadbareen@gmai.com>
2025-08-24 14:51:23 +0300

    Max_Number_of_K_Sum_Pairs

---
+++ Leetcode75/Max Number of K-Sum Pairs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode75
+{
+    public class Max_Number_of_K_Sum_Pairs
+    {
+        //https://leetcode.com/problems/max-number-of-k-sum-pairs/description/?envType=study-plan-v2&envId=leetcode-75
+
+        public int MaxOperations(int[] nums, int k)
+        {
+            int count=0;
+            Dictionary<int, int> x = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int searcher = k - nums[i];
+                if (!x.ContainsKey(searcher) )
+                {
+                    x[nums[i]]=i;
+                }
+                else
+                {
+                    count++;
+                    x.Remove(searcher);
+                }
+
+            }
+            return count;
+
+        }
+    }
+}
