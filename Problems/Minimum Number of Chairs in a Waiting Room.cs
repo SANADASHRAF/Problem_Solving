14e53449d308385f647908b0de3636527af2730d
SANADASHRAF <sanadbareen@gmai.com>
2025-08-26 11:55:49 +0300

    Minimum_Number_of_Chairs_in_a_Waiting_Room

---
+++ Problems/Minimum Number of Chairs in a Waiting Room.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    public class Minimum_Number_of_Chairs_in_a_Waiting_Room
+    {
+        //https://leetcode.com/problems/minimum-number-of-chairs-in-a-waiting-room/description/
+        public int MinimumChairs(string s)
+        {
+            int max = 0, count = 0;
+            foreach (var c in s)
+            {
+                if (c == 'E')
+                {
+                    count++;
+                    max = Math.Max(max, count);
+                }
+                else if (c == 'L')
+                {
+                    count--;
+                }
+            }
+            return max;
+        }
+
+        
+    }
+}
