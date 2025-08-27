f3516f5a631f48fb08888c215dc6821ed1ebbea9
SANADASHRAF <sanadbareen@gmai.com>
2025-08-27 14:01:46 +0300

    Generate_a_String_With_Characters_That_Have_Odd_Counts

---
+++ Problems/Generate a String With Characters That Have Odd Counts.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    public class Generate_a_String_With_Characters_That_Have_Odd_Counts
+    {
+        //https://leetcode.com/problems/generate-a-string-with-characters-that-have-odd-counts/
+        public string GenerateTheString(int n)
+        {
+            string result = "";
+            if (n % 2 == 0)
+            {
+                result = new string('a', n - 1) + 'b';
+            }
+            else
+            {
+                result = new string('a', n);
+            }
+            return result;
+        }
+    }
+}
--- Problems/Minimum Number of Chairs in a Waiting Room.cs
+++ Problems/Minimum Number of Chairs in a Waiting Room.cs
@@ -8,7 +8,7 @@ namespace Problems
 {
     public class Minimum_Number_of_Chairs_in_a_Waiting_Room
     {
-        //https://leetcode.com/problems/minimum-number-of-chairs-in-a-waiting-room/description/
+       //https://leetcode.com/problems/minimum-number-of-chairs-in-a-waiting-room/description/
         public int MinimumChairs(string s)
         {
             int max = 0, count = 0;
@@ -27,6 +27,26 @@ namespace Problems
             return max;
         }
 
-        
+        //public int MinimumChairs(string s)
+        //{
+        //    //  ELELEEL   ELEELEELLL
+        //    var queue = new Queue<char>();
+        //    foreach (var c in s)
+        //    {
+        //        if (c == 'E')
+        //        {
+        //            if (queue.Count > 0 && queue.Peek() == 'L')
+        //            {
+        //                queue.Dequeue();
+        //            }
+        //            else
+        //            {
+        //                queue.Enqueue(c);
+        //            }
+        //        }
+
+        //    }
+        //    return queue.Count;
+        //}
     }
 }
