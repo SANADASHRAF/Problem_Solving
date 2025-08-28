f22060e192e3354406fb29ed111d6901be6abe80
SANADASHRAF <sanadbareen@gmai.com>
2025-08-28 14:27:50 +0300

    Find the Winning Player in Coin Game

---
+++ Problems/Find the Winning Player in Coin Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    public class Find_the_Winning_Player_in_Coin_Game
+    {
+        //https://leetcode.com/problems/find-the-winning-player-in-coin-game/
+        public string WinningPlayer(int x, int y)
+        {
+            int count = -1;
+            while(x>0 && y>=4)
+            {
+                x -= 1;
+                y -= 4;
+                count++;
+            }
+            if((count & 1) == 0)
+            {
+                return "Alice";
+            }
+            return "Bob";
+        }
+
+    }
+}
