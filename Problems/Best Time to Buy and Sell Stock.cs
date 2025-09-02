ef8b4355e575bb9cb5432a5668d1fda342720a69
SANADASHRAF <sanadbareen@gmai.com>
2025-09-02 14:42:34 +0300

    Best_Time_to_Buy_and_Sell_Stock

---
+++ Problems/Best Time to Buy and Sell Stock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    public class Best_Time_to_Buy_and_Sell_Stock
+    {
+        //https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+        public int MaxProfit(int[] prices)
+        {
+            int minPrice = int.MaxValue;
+            int maxProfit = 0;
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (prices[i] < minPrice)
+                {
+                    minPrice = prices[i];
+                }
+                else if (prices[i] - minPrice > maxProfit)
+                {
+                    maxProfit = prices[i] - minPrice;
+                }
+            }
+            return maxProfit;
+        }
+    }
+}
