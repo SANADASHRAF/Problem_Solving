ac712a5ae9a2da85d96eb4970bfe892e1da21173
SANADASHRAF <sanadbareen@gmai.com>
2025-09-10 11:55:13 +0300

    Missing_Number

---
+++ Problems/Missing Number.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    public class Missing_Number
+    {
+        //https://leetcode.com/problems/missing-number/description/
+        public int MissingNumber(int[] nums)
+        {
+            nums=nums.OrderBy(x => x).ToArray();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != i)
+                    return i;
+            }
+            return nums.Length;
+        }
+    }
+}
