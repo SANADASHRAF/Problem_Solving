1b7cdd487c199db7a83e3c34b25e2c23f3ea8f52
SANADASHRAF <sanadbareen@gmai.com>
2025-12-16 11:46:54 +0200

    Longest_Substring_Without_Repeating_Characters

---
+++ Problems/Longest Substring Without Repeating Characters.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    public class Longest_Substring_Without_Repeating_Characters
+    {
+        //https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+        public int LengthOfLongestSubstring(string s)
+        {
+            int count = 0, loopcount = 0;
+
+            var map = new Dictionary<char, int>();
+            Queue<char> window = new Queue<char>();
+
+            foreach (char c in s)
+            {
+                while (map.ContainsKey(c))
+                {
+                    char removed = window.Dequeue();
+                    map.Remove(removed);
+                    loopcount--;
+                }
+
+                window.Enqueue(c);
+                map[c] = 1;
+                loopcount++;
+
+                count = Math.Max(count, loopcount);
+            }
+
+            return count;
+        }
+    }
+}
