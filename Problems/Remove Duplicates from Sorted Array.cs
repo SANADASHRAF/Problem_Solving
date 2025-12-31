77577ac053c6dae264d4bb73c71aeb54330a3045
SANADASHRAF <sanadbareen@gmai.com>
2025-12-31 13:53:29 +0200

    Remove_Duplicates_from_Sorted_Array

---
+++ Problems/Remove Duplicates from Sorted Array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    public class Remove_Duplicates_from_Sorted_Array
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            int notrepeatIndex = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[notrepeatIndex])
+                {
+                    notrepeatIndex++;
+                    nums[notrepeatIndex] = nums[i];
+                }
+            }
+            return notrepeatIndex + 1;
+        }
+    }
+}
